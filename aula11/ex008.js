c5b41ecffb3b83657e9f806a92b165f91c253b76
ViniAlOliv <viniciusaloli@gmail.com>
2021-01-12 22:09:49 -0300

    commit

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 55
+console.log(`A velocidade do seu carro é ${vel}mk/h!`)
+if (vel > 60) { //Condição simples
+    console.log('Você está acima do limite de velicidade. Rá!')
+}
+console.log('Dirija sempre com o cinto de segurança!')
+
